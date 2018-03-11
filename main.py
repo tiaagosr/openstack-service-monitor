ac8833099403fc7befde4699948a341510733007
Tiago Schoeping Reinert <tiago.schoeping@gmail.com>
2018-03-10 21:52:49 -0300

    Refactored Link Metering Module

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from database import DBSession
 
 persistence = DBSession(':memory:')
 #start_api_logging()
-link_metering = LinkMetering(iface='wlp2s0', filter='tcp', db=persistence, interval=2)
+link_metering = LinkMetering(iface='wlp2s0', db=persistence, interval=2)
 link_metering.start_monitoring()
