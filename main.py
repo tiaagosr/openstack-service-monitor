8b8c63ecfbd2ecfc59db6a6062469226f8d2b6a6
Tiago Schoeping Reinert <tiago.schoeping@gmail.com>
2018-03-13 14:56:22 -0300

    Api Logging Module

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import imports
 from modules.link_metering import LinkMetering
-#from modules.apilogging import start_api_logging
+from modules.api_logging import ApiLogging
 #from database import DBSession
 #wlp2s0
 #'enp0s20u3'
 
 #start_api_logging()
-link_metering = LinkMetering(iface='enp0s20u3', dbpath=':memory:', interval=3)
-link_metering.start_monitoring()
+api_logging = ApiLogging(iface='enp0s20u3', dbpath=':memory:', interval=3)
+api_logging.start_monitoring()
+
+#link_metering = LinkMetering(iface='enp0s20u3', dbpath=':memory:', interval=3)
+#link_metering.start_monitoring()
--- modules/api_logging.py
+++ modules/api_logging.py
@@ -4,14 +4,13 @@ from modules.definitions import MonitoringModule, DictionaryInit
 
 class ApiLogging(MonitoringModule):
 
-    def __init__(self, db, iface='wlp2s0', filter='tcp and (portrange 8773-8777 or port 5000 or port 9292 or port 80)', interval=10):
-        super().__init__(iface, filter, self.measure_packet, db)
+    def __init__(self, dbpath, iface='wlp2s0', filter='tcp and (port 8774 or port 5000 or port 35357 or port 8080 or port 9292 or port 8776 or port 9696)', interval=10):
+        super().__init__(iface, filter, self.measure_packet, dbpath)
         self.aux_thread_interval = interval
         self.dict = DictionaryInit()
         self.metering_result = self.dict.metering_dictionary()
         self.port_mapping = self.dict.api_ports()
         self.api_buffer = self.dict.port_dictionary()
-        self.init_persistance()
 
     def measure_packet(self, packet):
         if IP in packet:
@@ -26,7 +25,11 @@ class ApiLogging(MonitoringModule):
             self.api_buffer[dport].append(packet[Raw].load)
 
     def computate_and_persist(self):
-        return
+        for port in self.api_buffer:
+            tmp_list = list(self.api_buffer[port])
+            self.api_buffer[port] = []
+            for entry in tmp_list:
+                print("Destination: "+self.classify_port(port)+"\nContent: "+entry)
     
     def classify_port(self, port):
         if port in self.port_mapping:
@@ -34,16 +37,18 @@ class ApiLogging(MonitoringModule):
         return 'etc'
 
     def run(self):
+        #self.init_persistance()
         while not self.stopped.wait(self.aux_thread_interval):
             self.computate_and_persist()
 
     def start_monitoring(self):
-        print("Metering link usage, interval: "+str(self.aux_thread_interval))
-        self.start_sniffing({'offline':'test/testApi.pcap'})
+        print("Logging API requests")
+        self.start_sniffing()
         self.start()
 
     def _db_init_persistance(self, cursor):
         cursor.execute('''CREATE TABLE IF NOT EXISTS link_usage(id INTEGER PRIMARY KEY, interface VARCHAR(40), m_etc INTEGER, m_nova INTEGER, m_keystone INTEGER, m_glance INTEGER, m_cinder INTEGER, m_swift INTEGER, ignored_count INTEGER, time DATE DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'LOCALTIME')) )''')
 
     def init_persistance(self):
+        self.db.create_conn()
         self.db.wrap_access(self._db_init_persistance)
